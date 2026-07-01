5ac741c8b95646742e090cbc20c32a267baf024a
Magistika <andreas.hirmer@gmx.de>
2026-07-01 20:56:54 +0200

    Update sources.py

--- sources.py
+++ sources.py
@@ -9,21 +9,69 @@ HEADERS = {
 }
 
 SOURCES = [
+    # Deutschland
     {"name": "Prosatech", "url": "https://www.prosatech.de/search?sSearch=midea%20portasplit"},
     {"name": "HORNBACH", "url": "https://www.hornbach.de/suche/?searchTerm=midea%20portasplit"},
     {"name": "Globus Baumarkt", "url": "https://www.globus-baumarkt.de/search?sSearch=midea%20portasplit"},
     {"name": "OTTO", "url": "https://www.otto.de/suche/midea%20portasplit/"},
-    {"name": "Amazon PL", "url": "https://www.amazon.pl/s?k=midea+portasplit"},
-]
+    {"name": "BAUHAUS", "url": "https://www.bauhaus.info/suche/produkte?text=midea%20portasplit"},
+    {"name": "OBI", "url": "https://www.obi.de/suche/midea%20portasplit/"},
+    {"name": "hagebau", "url": "https://www.hagebau.de/suche/?q=midea%20portasplit"},
+    {"name": "toom", "url": "https://toom.de/suche/?q=midea%20portasplit"},
+    {"name": "Kaufland", "url": "https://www.kaufland.de/suche/?search_value=midea%20portasplit"},
+    {"name": "Conrad", "url": "https://www.conrad.de/de/search.html?search=midea%20portasplit"},
+    {"name": "Voelkner", "url": "https://www.voelkner.de/search?sSearch=midea%20portasplit"},
+    {"name": "Galaxus DE", "url": "https://www.galaxus.de/de/search?q=midea+portasplit"},
 
+    # Amazon
+    {"name": "Amazon DE", "url": "https://www.amazon.de/s?k=midea+portasplit"},
+    {"name": "Amazon PL", "url": "https://www.amazon.pl/s?k=midea+portasplit"},
+    {"name": "Amazon IT", "url": "https://www.amazon.it/s?k=midea+portasplit"},
+    {"name": "Amazon FR", "url": "https://www.amazon.fr/s?k=midea+portasplit"},
+    {"name": "Amazon ES", "url": "https://www.amazon.es/s?k=midea+portasplit"},
+    {"name": "Amazon NL", "url": "https://www.amazon.nl/s?k=midea+portasplit"},
+
+    # Marktplätze
+    {"name": "Kleinanzeigen", "url": "https://www.kleinanzeigen.de/s-midea-portasplit/k0"},
+    {"name": "eBay DE", "url": "https://www.ebay.de/sch/i.html?_nkw=midea+portasplit"},
+    {"name": "eBay FR", "url": "https://www.ebay.fr/sch/i.html?_nkw=midea+portasplit"},
+    {"name": "eBay IT", "url": "https://www.ebay.it/sch/i.html?_nkw=midea+portasplit"},
+    {"name": "eBay PL", "url": "https://www.ebay.pl/sch/i.html?_nkw=midea+portasplit"},
+
+    # Preisvergleich
+    {"name": "Idealo", "url": "https://www.idealo.de/preisvergleich/MainSearchProductCategory.html?q=midea+portasplit"},
+    {"name": "Google Shopping", "url": "https://www.google.com/search?tbm=shop&q=midea+portasplit"},
+
+    # Frankreich
+    {"name": "ManoMano FR", "url": "https://www.manomano.fr/s/midea-portasplit"},
+    {"name": "Leroy Merlin FR", "url": "https://www.leroymerlin.fr/recherche/?q=midea%20portasplit"},
+    {"name": "Brico Depot FR", "url": "https://www.bricodepot.fr/recherche/midea%20portasplit"},
+    {"name": "Fnac FR", "url": "https://www.fnac.com/SearchResult/ResultList.aspx?Search=midea+portasplit"},
+    {"name": "Rakuten FR", "url": "https://fr.shopping.rakuten.com/s/midea+portasplit"},
+
+    # Italien
+    {"name": "Bricoman IT", "url": "https://www.bricoman.it/search?q=midea%20portasplit"},
+
+    # Polen
+    {"name": "Allegro PL", "url": "https://allegro.pl/listing?string=midea%20portasplit"},
+    {"name": "Ceneo PL", "url": "https://www.ceneo.pl/;szukaj-midea+portasplit"},
+
+    # Niederlande
+    {"name": "Bol NL", "url": "https://www.bol.com/nl/nl/s/?searchtext=midea+portasplit"},
+
+    # Schweiz
+    {"name": "Galaxus CH", "url": "https://www.galaxus.ch/de/search?q=midea+portasplit"},
+    {"name": "Digitec CH", "url": "https://www.digitec.ch/de/search?q=midea+portasplit"},
+]
 
 def clean_text(text):
     return " ".join(text.split())
 
-
 def extract_price(text):
     text = text.replace(".", "").replace(",", ".")
+
     matches = re.findall(r"(\d+(?:\.\d{1,2})?)\s*€", text)
+
     prices = []
 
     for match in matches:
@@ -36,7 +84,6 @@ def extract_price(text):
 
     return min(prices) if prices else None
 
-
 def is_relevant(text):
     t = text.lower()
 
@@ -46,22 +93,7 @@ def is_relevant(text):
     if "portasplit" not in t:
         return False
 
-    bad_words = [
-        "zubehör",
-        "ersatzteil",
-        "filter",
-        "schlauch",
-        "fernbedienung",
-        "abdeckung",
-        "halterung",
-    ]
-
-    for word in bad_words:
-        if word in t:
-            return False
-
     return True
-
 
 def search_generic(source):
     offers = []
@@ -102,11 +134,9 @@ def search_generic(source):
             if a:
                 link = urljoin(source["url"], a["href"])
 
-            title = text[:120]
-
             offers.append({
                 "id": f"{source['name']}-{price}-{link}",
-                "title": title,
+                "title": text[:120],
                 "price": price,
                 "url": link,
                 "source": source["name"]
@@ -116,7 +146,6 @@ def search_generic(source):
         print(source["name"], "exception:", e)
 
     return offers
-
 
 def search_all_sources():
     results = []
