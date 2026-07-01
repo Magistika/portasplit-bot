11a25eae29f0e6a0456d22cc1319c7d029506c51
Magistika <andreas.hirmer@gmx.de>
2026-07-01 17:58:01 +0200

    notifier.py aktualisieren

--- notifier.py
+++ notifier.py
@@ -2,9 +2,16 @@ import requests
 from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
 def send(msg: str):
-    if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
-        print("Missing Telegram config")
-        return
-
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": TELEGRAM_CHAT_ID, "text": msg})
+
+    r = requests.post(
+        url,
+        data={
+            "chat_id": TELEGRAM_CHAT_ID,
+            "text": msg
+        },
+        timeout=30
+    )
+
+    print("Telegram Status:", r.status_code)
+    print("Telegram Response:", r.text)
