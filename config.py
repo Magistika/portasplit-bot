348b9862608277198ccb840dc80e897048d8b1ba
Magistika <andreas.hirmer@gmx.de>
2026-07-02 07:25:20 +0200

    Increase MAX_PRICE from 900 to 1000

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 SEARCH_QUERY = "Midea PortaSplit 12000 BTU"
-MAX_PRICE = 900
+MAX_PRICE = 1000
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
